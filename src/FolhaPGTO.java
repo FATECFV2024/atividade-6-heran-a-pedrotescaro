936228455ace25fda839f0030a9a4213d3241624
Pedro Antonio Silvestre Tescaro <133606054+pedrotescaro@users.noreply.github.com>
2024-11-21 00:12:40 -0300

    [feat] implementação dos métodos de cálculos e impressão para concluir o desafio

--- src/FolhaPGTO.java
+++ src/FolhaPGTO.java
@@ -1,5 +1,28 @@
 public class FolhaPGTO {
     public static void main(String[] args) {
-        
+        Mensalista men1, men2, men3;
+        Horista hora1, hora2;
+
+        // Empregados mensalistas
+        men1 = new Mensalista("Jose", "Rua abc", "Junior");
+        men1.calcularSalario();
+        men1.Imprimir();
+
+        men2 = new Mensalista("Ana", "Rua sem fim", "Senior");
+        men2.calcularSalario();
+        men2.Imprimir();
+
+        men3 = new Mensalista("Ana", "Rua sem fim", "");
+        men3.calcularSalario();
+        men3.Imprimir();
+
+        // Empregados horistas
+        hora1 = new Horista("Carlos", "Rua xyz", 20);
+        hora1.calcularSalario();
+        hora1.Imprimir();
+
+        hora2 = new Horista("Cristina", "Rua do centro", 100);
+        hora2.calcularSalario();
+        hora2.Imprimir();
     }
 }
